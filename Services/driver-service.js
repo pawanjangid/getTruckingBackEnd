e6b3f991374a2ed5476dfb61fdb1f92d29fa0300
Pawan Kumar Jangid <pawanjangid.ele@gmail.com>
2021-09-01 04:05:33 +0530

    vehiclestypeadmineditdelete

--- Services/driver-service.js
+++ Services/driver-service.js
@@ -3,14 +3,30 @@ const pool = require("../config/database");
 module.exports = {
     create: (data, callBack) => {
         pool.query(
-            "insert into driver(firstName, lastName, email, phone, password,createdAt) values(?,?,?,?,?,?)",
+            "insert into driver(fullName, email, phone, password,createdAt,Identification,emergency_contact_person,emergency_contact_number,driving_license_expiry,vehicle_type,vehicle_modal,License_plate_number,ID_card_front,ID_card_back,photo,Proof_of_Residency,Driving_license,Vehicle_License,vehicle_body,insurance,document_status) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
             [
                 data.firstName,
                 data.lastName,
                 data.email,
                 data.phone,
                 data.password,
-                Math.floor(Date.now() /1000)
+                Math.floor(Date.now() /1000),
+                data.Identification,
+                data.emergency_contact_person,
+                data.emergency_contact_number,
+                data.driving_license_expiry,
+                data.vehicle_type,
+                data.vehicle_modal,
+                data.License_plate_number,
+                data.ID_card_front,
+                data.ID_card_back,
+                data.photo,
+                data.Proof_of_Residency,
+                data.Driving_license,
+                data.Vehicle_License,
+                data.vehicle_body,
+                data.insurance,
+                data.document_status
             ],
             (error,results,fields) => {
                 if(error){
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const { getUser } = require('../Controller/user-controller');
 const { getDriver } = require('../Controller/driver-controller');
 const { addBatch, getBatch} = require('../Controller/batch-controller');
+const { listVehicle,addVehicle }  = require('../Controller/vehicle-controller');
 const router = require('express').Router();
 //const { checkToken } = require('../auth/token-admin-validation');
 
@@ -8,4 +9,6 @@ router.get("/users", getUser);
 router.get("/drivers", getDriver);
 router.get("/batch/:class_id", getBatch);
 router.post("/batch",addBatch);
+router.get("/vehicles",listVehicle);
+router.post("/vehicles",addVehicle);
 module.exports = router;
