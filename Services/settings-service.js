9b59e15822c7a37fd691b094a6a4ba1238018317
Pawan Kumar Jangid <pawanjangid.ele@gmail.com>
2021-10-22 00:15:51 +0530

    Update settings-service.js

--- Services/settings-service.js
+++ Services/settings-service.js
@@ -3,7 +3,7 @@ const pool = require("../config/database");
 module.exports = {
     saveData: (data, callBack) => {
         pool.query(
-            "update settings set dayColor=?,nightColor=?,dayBackground=?,nightBackground=?,updateTime=? where setting_id = 1",
+            "update Settings set dayColor=?,nightColor=?,dayBackground=?,nightBackground=?,updateTime=? where setting_id = 1",
             [
                 data.dayColor,
                 data.nightColor,
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     settingsData: (callBack) => {
-        pool.query("SELECT * FROM settings where setting_id = 1",[],
+        pool.query("SELECT * FROM Settings where setting_id = 1",[],
         (error,results,fields) => {
             if(error){
                 callBack(error);
