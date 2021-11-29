53dce1c7164a4d17bbb8955fdb28d8cd7e17dfa0
Pawan Kumar Jangid <pawanjangid.ele@gmail.com>
2021-11-29 21:18:29 +0530

    Update users-service.js

--- Services/users-service.js
+++ Services/users-service.js
@@ -43,7 +43,7 @@ module.exports = {
     },
 
     updateUser : (data,callBack) => {
-        pool.query("UPDATE users SET firstName = ?, lastName=?,email=?,phone=? WHERE user_id = ?", [data.firstName,data.lastName,data.email,data.phone,data.user_id],
+        pool.query("UPDATE users SET firstName = ?, lastName=?,email=?,phone=?,photoURL=? WHERE user_id = ?", [data.firstName,data.lastName,data.email,data.phone.data.photoURL,data.user_id],
             (error,results,fields)=>{
                 if(error){
                     callBack(error);
